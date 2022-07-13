0cbaf826b22a5cbe6c194e1247c2fc4c72070567
karan865 <108607705+karan865@users.noreply.github.com>
2022-07-13 15:23:42 +0530

    Updates

---
+++ precache-manifest.7ac9fe99d633f762a9ec144b28167217.js
@@ -0,0 +1,362 @@
+self.__precacheManifest = [
+  {
+    "revision": "591c2e8d798850f47790",
+    "url": "/painthub-2/static/css/main.3cff6b2e.chunk.css"
+  },
+  {
+    "revision": "591c2e8d798850f47790",
+    "url": "/painthub-2/static/js/main.591c2e8d.chunk.js"
+  },
+  {
+    "revision": "f396410328fc823aed37",
+    "url": "/painthub-2/static/js/1.f3964103.chunk.js"
+  },
+  {
+    "revision": "d2595400386ed9aac6b4",
+    "url": "/painthub-2/static/css/2.c17a4032.chunk.css"
+  },
+  {
+    "revision": "d2595400386ed9aac6b4",
+    "url": "/painthub-2/static/js/2.d2595400.chunk.js"
+  },
+  {
+    "revision": "064b4d4d1307249bdb50",
+    "url": "/painthub-2/static/js/3.064b4d4d.chunk.js"
+  },
+  {
+    "revision": "4674a36f27a19ef3da7d",
+    "url": "/painthub-2/static/js/runtime~main.4674a36f.js"
+  },
+  {
+    "revision": "b4bddb42f5dea6a075ea31f7f4f622a0",
+    "url": "/painthub-2/static/media/health-slider-2.b4bddb42.jpg"
+  },
+  {
+    "revision": "d6b9b55036d975d447b899fca02867a8",
+    "url": "/painthub-2/static/media/p1.d6b9b550.png"
+  },
+  {
+    "revision": "9e4ee8816d2894a39fcfe2cbecb53d96",
+    "url": "/painthub-2/static/media/shop-slider-3.9e4ee881.jpg"
+  },
+  {
+    "revision": "cb4313351c5d50d50ec46d3b7cc414f1",
+    "url": "/painthub-2/static/media/shop-slider-4.cb431335.jpg"
+  },
+  {
+    "revision": "bdea2f0ce6cc442c2147ce31727f1213",
+    "url": "/painthub-2/static/media/wall-slider-2.bdea2f0c.jpg"
+  },
+  {
+    "revision": "64263cd4155ef4f85acfa1125f6a9098",
+    "url": "/painthub-2/static/media/wall-slider-4.64263cd4.jpg"
+  },
+  {
+    "revision": "bbc219646a8cc9f264c8083c81e1a15c",
+    "url": "/painthub-2/static/media/health-slider-1.bbc21964.jpg"
+  },
+  {
+    "revision": "6b6ad28c1ef82d75e17a7ed0d868f866",
+    "url": "/painthub-2/static/media/health-slider-3.6b6ad28c.jpg"
+  },
+  {
+    "revision": "81a1805e4cb0cc1da736f9f7ae1cbc54",
+    "url": "/painthub-2/static/media/services-slider-2.81a1805e.jpeg"
+  },
+  {
+    "revision": "aadbdd70c2c5d110f031bdc92cf6388a",
+    "url": "/painthub-2/static/media/services-slider-4.aadbdd70.jpeg"
+  },
+  {
+    "revision": "8075fd52c5e14d7c2c86073b537c1cb4",
+    "url": "/painthub-2/static/media/p2.8075fd52.png"
+  },
+  {
+    "revision": "1541188092846865258f52469e06930b",
+    "url": "/painthub-2/static/media/p3.15411880.png"
+  },
+  {
+    "revision": "5294156cb4138251fe3f96e526ad18eb",
+    "url": "/painthub-2/static/media/p5.5294156c.jpg"
+  },
+  {
+    "revision": "3957a60d05572630349187b17fd532f7",
+    "url": "/painthub-2/static/media/p6.3957a60d.jpg"
+  },
+  {
+    "revision": "b20c44c003991b9b0b5f1fd3b7e9e11f",
+    "url": "/painthub-2/static/media/img1.b20c44c0.jpg"
+  },
+  {
+    "revision": "870ba80054d3c11a7f7190aab6d762c4",
+    "url": "/painthub-2/static/media/img2.870ba800.jpg"
+  },
+  {
+    "revision": "3686574e0c0135696f0ed87377b58dc5",
+    "url": "/painthub-2/static/media/img3.3686574e.jpg"
+  },
+  {
+    "revision": "a7fffe4f7e8e3f532ba743fe3c15088d",
+    "url": "/painthub-2/static/media/google-play-store.a7fffe4f.png"
+  },
+  {
+    "revision": "1e519bbab651a2fe1339db7750fdb015",
+    "url": "/painthub-2/static/media/designer-1.1e519bba.jpg"
+  },
+  {
+    "revision": "8938c0ca8067623154ab34c9d4ee61e9",
+    "url": "/painthub-2/static/media/designer-2.8938c0ca.jpg"
+  },
+  {
+    "revision": "ef3097f41985c220bcea6506dec9c6a6",
+    "url": "/painthub-2/static/media/designer-3.ef3097f4.jpg"
+  },
+  {
+    "revision": "03fb2dc305756bdc4366db1aa4ae8e75",
+    "url": "/painthub-2/static/media/most-populat-1.03fb2dc3.jpg"
+  },
+  {
+    "revision": "fc7b0573d9c42d105d86a27ec54913f7",
+    "url": "/painthub-2/static/media/most-populat-2.fc7b0573.jpg"
+  },
+  {
+    "revision": "0b581905e2a2297f785d5ebced729d0e",
+    "url": "/painthub-2/static/media/float-up-1.0b581905.jpg"
+  },
+  {
+    "revision": "14c1cea1d843cd8327a97effd971d9f7",
+    "url": "/painthub-2/static/media/float-up-2.14c1cea1.jpg"
+  },
+  {
+    "revision": "274c34229b52d237a1e611cf9dff0061",
+    "url": "/painthub-2/static/media/nilaya.274c3422.jpg"
+  },
+  {
+    "revision": "f7e1a4678af9309d3e4a82519f60f4e6",
+    "url": "/painthub-2/static/media/color-ideas.f7e1a467.jpg"
+  },
+  {
+    "revision": "9c46c7ad47e30ef5915fd8359ec0edd2",
+    "url": "/painthub-2/static/media/shop-slider-1.9c46c7ad.jpg"
+  },
+  {
+    "revision": "b37193904006eaaab1cec69f1b6b61c0",
+    "url": "/painthub-2/static/media/shop-slider-2.b3719390.jpg"
+  },
+  {
+    "revision": "470e240691bd199155baab0e80e7e066",
+    "url": "/painthub-2/static/media/shop-slider-5.470e2406.jpg"
+  },
+  {
+    "revision": "3d783b5578a1c4753961c36d295d713a",
+    "url": "/painthub-2/static/media/wall-slider-1.3d783b55.jpg"
+  },
+  {
+    "revision": "e22dbe05fd17a04e95db1dcf3112c68b",
+    "url": "/painthub-2/static/media/wall-slider-3.e22dbe05.jpg"
+  },
+  {
+    "revision": "4d3bee1ba65a85b80b998ca9b613ce25",
+    "url": "/painthub-2/static/media/wall-slider-5.4d3bee1b.jpg"
+  },
+  {
+    "revision": "2385e5dde9628164368cf22c8be42335",
+    "url": "/painthub-2/static/media/health-1.2385e5dd.png"
+  },
+  {
+    "revision": "12c92dab01b41701405c7726a50914b2",
+    "url": "/painthub-2/static/media/health-2.12c92dab.png"
+  },
+  {
+    "revision": "4a4440fe1b6cec9dcf65660e184f993d",
+    "url": "/painthub-2/static/media/health-3.4a4440fe.png"
+  },
+  {
+    "revision": "988e8afb8cb39cdb7e444a78ab9b66cd",
+    "url": "/painthub-2/static/media/why-anian-apint-1.988e8afb.png"
+  },
+  {
+    "revision": "cfbbb7b92d55f87175b5ea3dfdb87370",
+    "url": "/painthub-2/static/media/why-anian-apint-2.cfbbb7b9.png"
+  },
+  {
+    "revision": "eebde6cbbe15ca3cf55e008446d7276b",
+    "url": "/painthub-2/static/media/shop-online-1.eebde6cb.jpg"
+  },
+  {
+    "revision": "7d343d1a5105574f4130e41c0faf66cb",
+    "url": "/painthub-2/static/media/shop-online-2.7d343d1a.jpg"
+  },
+  {
+    "revision": "66e52cd4c96d87e5f61fe2415cd2b63c",
+    "url": "/painthub-2/static/media/shop-online-3.66e52cd4.jpg"
+  },
+  {
+    "revision": "a519312365acb175448ade7ae1872784",
+    "url": "/painthub-2/static/media/color-ideas-2.a5193123.jpg"
+  },
+  {
+    "revision": "59f9902ce27ff730e1dd806aeee8a9c2",
+    "url": "/painthub-2/static/media/color-ideas-3.59f9902c.jpg"
+  },
+  {
+    "revision": "f3abf26fc253f30b7c6e78757ab4cd46",
+    "url": "/painthub-2/static/media/services-offer-1.f3abf26f.jpg"
+  },
+  {
+    "revision": "161d110d9681ce70107d51a13722166d",
+    "url": "/painthub-2/static/media/services-offer-2.161d110d.jpg"
+  },
+  {
+    "revision": "c47e656a4dce743cc93688b1529f3e62",
+    "url": "/painthub-2/static/media/services-offer-3.c47e656a.jpg"
+  },
+  {
+    "revision": "2dc3749ff41a402da903eb213d203191",
+    "url": "/painthub-2/static/media/services-offer-4.2dc3749f.jpg"
+  },
+  {
+    "revision": "776e144e04715acf498ce1426fc15787",
+    "url": "/painthub-2/static/media/services-offer-5.776e144e.jpg"
+  },
+  {
+    "revision": "bf17e2337a6b3ec0fdedd747a26c5818",
+    "url": "/painthub-2/static/media/services-offer-6.bf17e233.jpg"
+  },
+  {
+    "revision": "eaf63003671f12a0c7846cc3c897bc69",
+    "url": "/painthub-2/static/media/services-offer-7.eaf63003.jpg"
+  },
+  {
+    "revision": "4b40ebe0d40126e3ec0cc96aefc66568",
+    "url": "/painthub-2/static/media/services-offer-8.4b40ebe0.jpg"
+  },
+  {
+    "revision": "3bd418c3119760bbb4cd51b9e6f1fba4",
+    "url": "/painthub-2/static/media/icon-image-1.3bd418c3.png"
+  },
+  {
+    "revision": "6a4f968817ebcb5691008434ec512e4b",
+    "url": "/painthub-2/static/media/icon-image-3.6a4f9688.png"
+  },
+  {
+    "revision": "873db345a7f4f16dddb757048ce9b9fd",
+    "url": "/painthub-2/static/media/icon-image-4.873db345.png"
+  },
+  {
+    "revision": "c5af5130de89bd3fee0a212e61ec5e4a",
+    "url": "/painthub-2/static/media/three-section-4.c5af5130.jpg"
+  },
+  {
+    "revision": "402f29a3ada1bd48ab3a1aeccb026f39",
+    "url": "/painthub-2/static/media/three-section-5.402f29a3.jpg"
+  },
+  {
+    "revision": "8958201ac77cc4231dc4040dafbd6994",
+    "url": "/painthub-2/static/media/three-section-6.8958201a.jpg"
+  },
+  {
+    "revision": "5f65237dd2f6550b3fe9f919540a1a58",
+    "url": "/painthub-2/static/media/three-section-7.5f65237d.jpg"
+  },
+  {
+    "revision": "e9008398498bdc3d768201bc78710f80",
+    "url": "/painthub-2/static/media/three-section-8.e9008398.jpg"
+  },
+  {
+    "revision": "ec6d04843885d874d0426cf975fcae47",
+    "url": "/painthub-2/static/media/sanitization.ec6d0484.jpg"
+  },
+  {
+    "revision": "8459eb9febeb273309cdb09007035552",
+    "url": "/painthub-2/static/media/services-slider.8459eb9f.jpeg"
+  },
+  {
+    "revision": "e432f4db3b3563a565d4d2fe32390736",
+    "url": "/painthub-2/static/media/services-slider-3.e432f4db.jpeg"
+  },
+  {
+    "revision": "6fd1e98ad79898a3c3744449449c0017",
+    "url": "/painthub-2/static/media/services-slider-5.6fd1e98a.jpeg"
+  },
+  {
+    "revision": "8da5cba060b0e782d081f06da279402d",
+    "url": "/painthub-2/static/media/shop-1.8da5cba0.jpg"
+  },
+  {
+    "revision": "93d72f4647ea7a76bd2e690388f78915",
+    "url": "/painthub-2/static/media/shop-2.93d72f46.jpg"
+  },
+  {
+    "revision": "5581e5a555c9498831ab172ba59d0e58",
+    "url": "/painthub-2/static/media/shop-3.5581e5a5.jpg"
+  },
+  {
+    "revision": "321d564202680f0be7c785efd7e5069a",
+    "url": "/painthub-2/static/media/shop-4.321d5642.jpg"
+  },
+  {
+    "revision": "8ca728671366218a3f84c67e1e560d59",
+    "url": "/painthub-2/static/media/shop-5.8ca72867.jpg"
+  },
+  {
+    "revision": "b9c1f42ecbfcb8a8c90184efd82d0256",
+    "url": "/painthub-2/static/media/shop-6.b9c1f42e.jpg"
+  },
+  {
+    "revision": "bdc7a48bf7f31aea8f5ec0f8734f1fdf",
+    "url": "/painthub-2/static/media/shop-7.bdc7a48b.jpg"
+  },
+  {
+    "revision": "6fe288a8292e976c3b6d6af7699d8f33",
+    "url": "/painthub-2/static/media/shop-8.6fe288a8.jpg"
+  },
+  {
+    "revision": "40ee04db7d5dd5c5b8c622ccc86e9552",
+    "url": "/painthub-2/static/media/shop-9.40ee04db.jpg"
+  },
+  {
+    "revision": "cda23cbab812cf0dbedfbede8eaa7ea7",
+    "url": "/painthub-2/static/media/sticker-1.cda23cba.jpg"
+  },
+  {
+    "revision": "a1157beadfff8eed4a3316e8539d9634",
+    "url": "/painthub-2/static/media/sticker-2.a1157bea.jpg"
+  },
+  {
+    "revision": "2c69c3cd9793553798b653b2bc8f07f2",
+    "url": "/painthub-2/static/media/sticker-4.2c69c3cd.jpg"
+  },
+  {
+    "revision": "c3c1f16c99b33a1ec0d3e1d0c1692c2e",
+    "url": "/painthub-2/static/media/painting-tool.c3c1f16c.jpg"
+  },
+  {
+    "revision": "0c5aa624cef2ac5844ab51c9fb69fc2d",
+    "url": "/painthub-2/static/media/sofa.0c5aa624.jpg"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "/painthub-2/static/media/revicons.04eb8fc5.woff"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "/painthub-2/static/media/revicons.17629a5d.ttf"
+  },
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "/painthub-2/static/media/revicons.2feb69cc.eot"
+  },
+  {
+    "revision": "85099374616d7b25c11e03d3b94b4d4e",
+    "url": "/painthub-2/static/media/overlap-1.85099374.jpg"
+  },
+  {
+    "revision": "c49c009acef357d07b8624cf654b2103",
+    "url": "/painthub-2/static/media/color-spectra.c49c009a.jpg"
+  },
+  {
+    "revision": "1567089df14098fe23efa045a4d7c93c",
+    "url": "/painthub-2/index.html"
+  }
+];
